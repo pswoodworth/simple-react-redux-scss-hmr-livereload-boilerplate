2ff821d3c95688d5bda834f9e59c2962cc5a592b
Parker Woodworth <parker@parkers-mbp.home>
2017-12-23 10:17:26 +0000

    switched to eslint

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import Layout from './Layout';
 import Hey from './Hey';
 
 
-export default function(history){
-  return(
+export default function (history) {
+  return (
     <Router history={history}>
       <Route path="/" component={Layout}>
-        <IndexRoute component={Hey}/>
+        <IndexRoute component={Hey} />
       </Route>
     </Router>
   );
 }
-
-
-
